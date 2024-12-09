4275840f5c28938dccbf629b12c3534733d5a4e4
iamhrhp <hrhp007@gmail.com>
2024-12-10 00:44:30 +0530

    Added responsive code

---
+++ src/utils/ThemeToggle.ts
@@ -0,0 +1,14 @@
+// theme.js
+const lightTheme = {
+    backgroundColor: '#FFFFFF',
+    textColor: '#000000',
+    // other light mode styles
+  };
+  
+  const darkTheme = {
+    backgroundColor: '#000000',
+    textColor: '#FFFFFF',
+    // other dark mode styles
+  };
+  
+  export {lightTheme, darkTheme};
